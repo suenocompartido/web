74cacbd0542a369ac70c3038757e05713b2528d0
Jose Manuel Alcalde Llergo <josemanuelalcaldellergo@gmail.com>
2026-05-12 23:56:25 +0200

    Fix: basename /web añadido a las rutas

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,22 @@
-import { createBrowserRouter } from 'react-router';
-import { RootLayout } from './layouts/RootLayout';
-import { Home } from './pages/Home';
-import { Transparencia } from './pages/Transparencia';
-import { PrivacyPolicy } from './pages/PrivacyPolicy';
+import { createBrowserRouter } from "react-router";
+import { RootLayout } from "./layouts/RootLayout";
+import { Home } from "./pages/Home";
+import { Transparencia } from "./pages/Transparencia";
+import { PrivacyPolicy } from "./pages/PrivacyPolicy";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: RootLayout,
+      children: [
+        { index: true, Component: Home },
+        { path: "transparencia", Component: Transparencia },
+        { path: "privacidad", Component: PrivacyPolicy },
+      ],
+    },
+  ],
   {
-    path: '/',
-    Component: RootLayout,
-    children: [
-      { index: true, Component: Home },
-      { path: 'transparencia', Component: Transparencia },
-      { path: 'privacidad', Component: PrivacyPolicy },
-    ],
+    basename: "/web",
   },
-]);
+);
